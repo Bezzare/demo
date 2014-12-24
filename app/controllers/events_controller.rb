c7e1b7bcede3a3a33fee9ea3d0f6c1820e8c7738
Bezzare <bezzarelin@gmail.com>
2014-12-24 23:37:34 +0800

    1)fix bug of before_action set_event, 2) error message display for name field verification  in new/update actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,32 +3,35 @@ class EventsController < ApplicationController
     before_action :set_event, :only => [ :show, :edit, :update, :destroy ]
 
     def index
-	@events = Event.all
+	   @events = Event.all
     end
 
     def new
-	@event = Event.new
+	   @event = Event.new
     end
 
     def create
-	#@event = Event.new(params[:event])
-	@event = Event.new(event_params)
-	@event.save
-
-	redirect_to :action => :index
+       @event = Event.new(event_params)
+    	if @event.save
+            redirect_to :action => :index
+        else
+            render :action => :new
+        end
     end
 
     def show
-    @page_title = @event.name
+        @page_title = @event.name
     end
 
     def edit
     end
 
     def update
-        @event.update(event_params)
-
-        redirect_to :action => :show, :id => @event
+        if @event.update(event_params)
+            redirect_to :action => :show, :id => @event
+        else
+            render :action => :edit
+        end
     end
 
     def destroy
@@ -40,10 +43,10 @@ class EventsController < ApplicationController
     private
 
     def event_params
-	params.require(:event).permit(:name, :description)
+	   params.require(:event).permit(:name, :description)
     end
 
     def set_event
-        @event = Event.find { params[:id]  }
+        @event = Event.find (params[:id])
     end
 end

5a9d5879f99f820e60065450c9a9c63fe5f33fcb
Bezzare <bezzarelin@gmail.com>
2015-01-07 12:48:24 +0800

    generate attendee model belonging to event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106083805) do
+ActiveRecord::Schema.define(version: 20150107031536) do
+
+  create_table "attendees", force: true do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", force: true do |t|
     t.string   "name"
